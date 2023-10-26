7b9215d49ac68f892b0ce9bba11e6bb5f7b95291
Galant <dungkcr17@gmail.com>
2023-10-26 10:44:19 +0700

    Them Form Tim kiem doc gia

---
+++ QLTV/WindowsFormsApp2/timKiemDocGia.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class timKiemDocGia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.đăngXuấtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.danhMụcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýSáchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýĐộcGiảToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýNhânViênToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mượnTrảSáchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lậpPhiếuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tìmKiếmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sáchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.độcGiảToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.báoCáoThốngKêToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xuấtExcelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvSReader = new System.Windows.Forms.DataGridView();
+            this.rbFollowName = new System.Windows.Forms.RadioButton();
+            this.rbFollowIDReader = new System.Windows.Forms.RadioButton();
+            this.btnExitSReader = new System.Windows.Forms.Button();
+            this.btnSReader = new System.Windows.Forms.Button();
+            this.txtSReader = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSReader)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hToolStripMenuItem,
+            this.danhMụcToolStripMenuItem,
+            this.mượnTrảSáchToolStripMenuItem,
+            this.tìmKiếmToolStripMenuItem,
+            this.báoCáoThốngKêToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(939, 33);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hToolStripMenuItem
+            // 
+            this.hToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.đăngXuấtToolStripMenuItem,
+            this.thoátToolStripMenuItem});
+            this.hToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.system2;
+            this.hToolStripMenuItem.Name = "hToolStripMenuItem";
+            this.hToolStripMenuItem.Size = new System.Drawing.Size(127, 29);
+            this.hToolStripMenuItem.Text = "Hệ thống";
+            // 
+            // đăngXuấtToolStripMenuItem
+            // 
+            this.đăngXuấtToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.iconexit;
+            this.đăngXuấtToolStripMenuItem.Name = "đăngXuấtToolStripMenuItem";
+            this.đăngXuấtToolStripMenuItem.Size = new System.Drawing.Size(219, 34);
+            this.đăngXuấtToolStripMenuItem.Text = "Đăng xuất";
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.ext;
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(219, 34);
+            this.thoátToolStripMenuItem.Text = "Thoát";
+            // 
+            // danhMụcToolStripMenuItem
+            // 
+            this.danhMụcToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quảnLýSáchToolStripMenuItem,
+            this.quảnLýĐộcGiảToolStripMenuItem,
+            this.quảnLýNhânViênToolStripMenuItem});
+            this.danhMụcToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.list;
+            this.danhMụcToolStripMenuItem.Name = "danhMụcToolStripMenuItem";
+            this.danhMụcToolStripMenuItem.Size = new System.Drawing.Size(133, 29);
+            this.danhMụcToolStripMenuItem.Text = "Danh mục";
+            // 
+            // quảnLýSáchToolStripMenuItem
+            // 
+            this.quảnLýSáchToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.addbook;
+            this.quảnLýSáchToolStripMenuItem.Name = "quảnLýSáchToolStripMenuItem";
+            this.quảnLýSáchToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D1)));
+            this.quảnLýSáchToolStripMenuItem.Size = new System.Drawing.Size(317, 34);
+            this.quảnLýSáchToolStripMenuItem.Text = "Quản lý sách";
+            // 
+            // quảnLýĐộcGiảToolStripMenuItem
+            // 
+            this.quảnLýĐộcGiảToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.addhuman;
+            this.quảnLýĐộcGiảToolStripMenuItem.Name = "quảnLýĐộcGiảToolStripMenuItem";
+            this.quảnLýĐộcGiảToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D2)));
+            this.quảnLýĐộcGiảToolStripMenuItem.Size = new System.Drawing.Size(317, 34);
+            this.quảnLýĐộcGiảToolStripMenuItem.Text = "Quản lý độc giả";
+            // 
+            // quảnLýNhânViênToolStripMenuItem
+            // 
+            this.quảnLýNhânViênToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.addstaff;
+            this.quảnLýNhânViênToolStripMenuItem.Name = "quảnLýNhânViênToolStripMenuItem";
+            this.quảnLýNhânViênToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D3)));
+            this.quảnLýNhânViênToolStripMenuItem.Size = new System.Drawing.Size(317, 34);
+            this.quảnLýNhânViênToolStripMenuItem.Text = "Quản lý nhân viên";
+            // 
+            // mượnTrảSáchToolStripMenuItem
+            // 
+            this.mượnTrảSáchToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lậpPhiếuToolStripMenuItem});
+            this.mượnTrảSáchToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.brrowbook;
+            this.mượnTrảSáchToolStripMenuItem.Name = "mượnTrảSáchToolStripMenuItem";
+            this.mượnTrảSáchToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.mượnTrảSáchToolStripMenuItem.Size = new System.Drawing.Size(166, 29);
+            this.mượnTrảSáchToolStripMenuItem.Text = "Mượn trả sách";
+            // 
+            // lậpPhiếuToolStripMenuItem
+            // 
+            this.lậpPhiếuToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.list;
+            this.lậpPhiếuToolStripMenuItem.Name = "lậpPhiếuToolStripMenuItem";
+            this.lậpPhiếuToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.lậpPhiếuToolStripMenuItem.Size = new System.Drawing.Size(255, 34);
+            this.lậpPhiếuToolStripMenuItem.Text = "Lập phiếu";
+            // 
+            // tìmKiếmToolStripMenuItem
+            // 
+            this.tìmKiếmToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sáchToolStripMenuItem,
+            this.độcGiảToolStripMenuItem});
+            this.tìmKiếmToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.search2;
+            this.tìmKiếmToolStripMenuItem.Name = "tìmKiếmToolStripMenuItem";
+            this.tìmKiếmToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
+            this.tìmKiếmToolStripMenuItem.Size = new System.Drawing.Size(124, 29);
+            this.tìmKiếmToolStripMenuItem.Text = "Tìm kiếm";
+            // 
+            // sáchToolStripMenuItem
+            // 
+            this.sáchToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.brrowbook;
+            this.sáchToolStripMenuItem.Name = "sáchToolStripMenuItem";
+            this.sáchToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
+            this.sáchToolStripMenuItem.Size = new System.Drawing.Size(238, 34);
+            this.sáchToolStripMenuItem.Text = "Sách";
+            // 
+            // độcGiảToolStripMenuItem
+            // 
+            this.độcGiảToolStripMenuItem.Image = global::WindowsFormsApp2.Properties.Resources.addhuman;
+            this.độcGiảToolStripMenuItem.Name = "độcGiảToolStripMenuItem";
+            this.độcGiảToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.U)));
+            this.độcGiảToolStripMenuItem.Size = new System.Drawing.Size(238, 34);
+            this.độcGiảToolStripMenuItem.Text = "Độc giả";
+            // 
+            // báoCáoThốngKêToolStripMenuItem
+            // 
+            this.báoCáoThốngKêToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.xuấtExcelToolStripMenuItem});
+            this.báoCáoThốngKêToolStripMenuItem.Name = "báoCáoThốngKêToolStripMenuItem";
+            this.báoCáoThốngKêToolStripMenuItem.Size = new System.Drawing.Size(167, 29);
+            this.báoCáoThốngKêToolStripMenuItem.Text = "Báo cáo thống kê";
+            // 
+            // xuấtExcelToolStripMenuItem
+            // 
+            this.xuấtExcelToolStripMenuItem.Name = "xuấtExcelToolStripMenuItem";
+            this.xuấtExcelToolStripMenuItem.Size = new System.Drawing.Size(193, 34);
+            this.xuấtExcelToolStripMenuItem.Text = "Xuất Excel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(314, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tìm Kiếm Đọc Giả";
+            // 
+            // dgvSReader
+            // 
+            this.dgvSReader.AllowUserToAddRows = false;
+            this.dgvSReader.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSReader.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSReader.Location = new System.Drawing.Point(8, 242);
+            this.dgvSReader.Name = "dgvSReader";
+            this.dgvSReader.RowHeadersWidth = 62;
+            this.dgvSReader.RowTemplate.Height = 28;
+            this.dgvSReader.Size = new System.Drawing.Size(921, 338);
+            this.dgvSReader.TabIndex = 5;
+            // 
+            // rbFollowName
+            // 
+            this.rbFollowName.AutoSize = true;
+            this.rbFollowName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbFollowName.Location = new System.Drawing.Point(44, 82);
+            this.rbFollowName.Name = "rbFollowName";
+            this.rbFollowName.Size = new System.Drawing.Size(179, 24);
+            this.rbFollowName.TabIndex = 4;
+            this.rbFollowName.TabStop = true;
+            this.rbFollowName.Text = "Theo Tên Đọc Giả";
+            this.rbFollowName.UseVisualStyleBackColor = true;
+            // 
+            // rbFollowIDReader
+            // 
+            this.rbFollowIDReader.AutoSize = true;
+            this.rbFollowIDReader.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbFollowIDReader.Location = new System.Drawing.Point(42, 35);
+            this.rbFollowIDReader.Name = "rbFollowIDReader";
+            this.rbFollowIDReader.Size = new System.Drawing.Size(173, 24);
+            this.rbFollowIDReader.TabIndex = 3;
+            this.rbFollowIDReader.TabStop = true;
+            this.rbFollowIDReader.Text = "Theo Mã Đọc Giả";
+            this.rbFollowIDReader.UseVisualStyleBackColor = true;
+            // 
+            // btnExitSReader
+            // 
+            this.btnExitSReader.Location = new System.Drawing.Point(797, 56);
+            this.btnExitSReader.Name = "btnExitSReader";
+            this.btnExitSReader.Size = new System.Drawing.Size(75, 32);
+            this.btnExitSReader.TabIndex = 2;
+            this.btnExitSReader.Text = "Thoát";
+            this.btnExitSReader.UseVisualStyleBackColor = true;
+            this.btnExitSReader.Click += new System.EventHandler(this.btnExitSReader_Click);
+            // 
+            // btnSReader
+            // 
+            this.btnSReader.Location = new System.Drawing.Point(683, 56);
+            this.btnSReader.Name = "btnSReader";
+            this.btnSReader.Size = new System.Drawing.Size(75, 32);
+            this.btnSReader.TabIndex = 1;
+            this.btnSReader.Text = "Tìm";
+            this.btnSReader.UseVisualStyleBackColor = true;
+            this.btnSReader.Click += new System.EventHandler(this.btnSReader_Click);
+            // 
+            // txtSReader
+            // 
+            this.txtSReader.Location = new System.Drawing.Point(268, 59);
+            this.txtSReader.Name = "txtSReader";
+            this.txtSReader.Size = new System.Drawing.Size(368, 26);
+            this.txtSReader.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rbFollowName);
+            this.panel1.Controls.Add(this.rbFollowIDReader);
+            this.panel1.Controls.Add(this.btnExitSReader);
+            this.panel1.Controls.Add(this.btnSReader);
+            this.panel1.Controls.Add(this.txtSReader);
+            this.panel1.Location = new System.Drawing.Point(8, 103);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(921, 132);
+            this.panel1.TabIndex = 4;
+            // 
+            // timKiemDocGia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(939, 591);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvSReader);
+            this.Controls.Add(this.panel1);
+            this.Name = "timKiemDocGia";
+            this.Text = "timKiemDocGia";
+            this.Load += new System.EventHandler(this.timKiemDocGia_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSReader)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem đăngXuấtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem danhMụcToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýSáchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýĐộcGiảToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýNhânViênToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mượnTrảSáchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lậpPhiếuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tìmKiếmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sáchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem độcGiảToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem báoCáoThốngKêToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xuấtExcelToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvSReader;
+        private System.Windows.Forms.RadioButton rbFollowName;
+        private System.Windows.Forms.RadioButton rbFollowIDReader;
+        private System.Windows.Forms.Button btnExitSReader;
+        private System.Windows.Forms.Button btnSReader;
+        private System.Windows.Forms.TextBox txtSReader;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ QLTV/WindowsFormsApp2/timKiemDocGia.cs
@@ -0,0 +1,71 @@
+﻿using DAL.Models;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class timKiemDocGia : Form
+    {
+        public timKiemDocGia()
+        {
+            InitializeComponent();
+        }
+
+        private void timKiemDocGia_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSReader_Click(object sender, EventArgs e)
+        {
+            string keyword = txtSReader.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(keyword))
+            {
+                MessageBox.Show("Vui lòng nhập từ khóa tìm kiếm.", "Lỗi");
+                return;
+            }
+
+
+            List<DocGia> results = new List<DocGia>();
+
+            using (Model1 context = new Model1())
+            {
+                if (rbFollowName.Checked)
+                {
+                    results = context.DocGia.Where(dg => dg.TenDocGia.Contains(keyword)).ToList();
+                }
+                else if (rbFollowIDReader.Checked)
+                {
+                    results = context.DocGia.Where(dg => dg.MaDocGia.Contains(keyword)).ToList();
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng chọn loại tìm kiếm.", "Lỗi");
+                    return;
+                }
+            }
+
+            if (results.Count == 0)
+            {
+                MessageBox.Show("Không tìm thấy đọc giả nào khớp với từ khóa.", "Thông báo");
+                return;
+            }
+
+            dgvSReader.DataSource = results;
+        }
+
+        private void btnExitSReader_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
