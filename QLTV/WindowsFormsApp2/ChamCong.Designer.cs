24d73b9c181a983958cab996a9e8165e4e32d98c
Galant <dungkcr17@gmail.com>
2023-11-02 12:43:06 +0700

    Ban cuoi

--- QLTV/WindowsFormsApp2/ChamCong.Designer.cs
+++ QLTV/WindowsFormsApp2/ChamCong.Designer.cs
@@ -52,11 +52,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(535, 37);
+            this.label1.Location = new System.Drawing.Point(576, 37);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(166, 32);
+            this.label1.Size = new System.Drawing.Size(117, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Chấm công";
             // 
